56c1b521140b73b26fc12522d7b544e81c901e30
Kevin R. Barnes <vinbarnes@gmail.com>
2013-03-15 17:39:37 -0700

    kickstart my heart

---
+++ js/en.wikipedia.org.js
@@ -0,0 +1,2 @@
+$('div#content').css({'width': '650px', 'font-size': '20px', 'font-family': 'Georgia', 'color': '#222', 'line-height': '1.5', 'background-color': '#F4F4F4'});
+$('table#toc').css({'position': 'absolute', 'top': '0px', 'left': '700px'});
